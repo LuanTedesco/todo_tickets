c155c27ead6d78e29931fc4996f78dcbee18e46a
Luan Tedesco <luanfelipe2040@gmail.com>
2023-08-04 15:26:46 -0300

    fixed levels

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 User.create(email: 'admin@admin.com', password: '123456', password_confirmation: '123456')
 
 Column.create(title: 'Aguardando', description: 'Aguardando', level: 1, visible: true)
-Column.create(title: 'Desenvolvendo', description: 'Desenvolvendo', level: 1, visible: true)
-Column.create(title: 'Testando', description: 'Testando', level: 1, visible: true)
-Column.create(title: 'Pronto', description: 'Pronto', level: 1, visible: true)
-Column.create(title: 'Arquivo Morto', description: 'Arquivo Morto', level: 1, visible: false)
+Column.create(title: 'Desenvolvendo', description: 'Desenvolvendo', level: 2, visible: true)
+Column.create(title: 'Testando', description: 'Testando', level: 3, visible: true)
+Column.create(title: 'Pronto', description: 'Pronto', level: 4, visible: true)
+Column.create(title: 'Arquivo Morto', description: 'Arquivo Morto', level: 5, visible: false)
 
 Priority.create(name: 'Baixa', description: 'Baixa', level: 1)
 Priority.create(name: 'Média', description: 'Media', level: 2)
