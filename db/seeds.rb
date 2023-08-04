fe26374150cfe77c057295429a667649a826532c
Luan Tedesco <luanfelipe2040@gmail.com>
2023-08-04 11:16:11 -0300

    added priority registers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@ Column.create(title: 'Desenvolvendo', description: 'Desenvolvendo', level: 1, vi
 Column.create(title: 'Testando', description: 'Testando', level: 1, visible: true)
 Column.create(title: 'Pronto', description: 'Pronto', level: 1, visible: true)
 Column.create(title: 'Arquivo Morto', description: 'Arquivo Morto', level: 1, visible: false)
+
+Priority.create(name: 'Baixa', description: 'Baixa', level: 1)
+Priority.create(name: 'Média', description: 'Media', level: 2)
+Priority.create(name: 'Alta', description: 'Alta', level: 3)
+Priority.create(name: 'Urgente', description: 'Urgente', level: 4)
