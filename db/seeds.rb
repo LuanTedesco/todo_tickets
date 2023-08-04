e2f121519f650c2d8fd6799b0354290b262d0d87
Luan Tedesco <luanfelipe2040@gmail.com>
2023-08-04 10:43:25 -0300

    added default values for columns

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'admin@admin.com', password: '123456', password_confirmation: '123456')
+
+Column.create(title: 'Aguardando', description: 'Aguardando', level: 1, visible: true)
+Column.create(title: 'Desenvolvendo', description: 'Desenvolvendo', level: 1, visible: true)
+Column.create(title: 'Testando', description: 'Testando', level: 1, visible: true)
+Column.create(title: 'Pronto', description: 'Pronto', level: 1, visible: true)
+Column.create(title: 'Arquivo Morto', description: 'Arquivo Morto', level: 1, visible: false)
