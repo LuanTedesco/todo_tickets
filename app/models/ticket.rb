6e091b518a64096bc98cb676972777722ed21aac
Luan Tedesco <luanfelipe2040@gmail.com>
2023-10-10 12:00:21 -0300

    removed files to create a ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,7 @@ class Ticket < ApplicationRecord
   belongs_to :priority
   belongs_to :column
   belongs_to :departament
-  has_many_attached :files
+  # has_many_attached :files
 
-  validates :files, attached: true, limit: { min: 1, max: 3 }, content_type: %i[png jpg jpeg mp3 pdf]
+  # validates :files, attached: true, limit: { min: 1, max: 3 }, content_type: %i[png jpg jpeg mp3 pdf]
 end
