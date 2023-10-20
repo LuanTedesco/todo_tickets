3b69744e060812190fc27a5103530da3b730a954
Luan Tedesco <luanfelipe2040@gmail.com>
2023-10-20 09:36:44 -0300

    change code sessions_controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,14 @@
-# frozen_string_literal: true
+class SessionsController < Devise::SessionsController
+  before_action :check_status, only: [:create]
 
-class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  private
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def check_status
+    user = User.find_by(email: params[:user][:email])
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+    return unless user && !user.status
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    flash[:alert] = 'Sua conta está desativada. Entre em contato com o suporte.'
+    redirect_to new_user_session_path
+  end
 end
