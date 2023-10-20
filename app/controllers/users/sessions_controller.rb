755a9b61428793e0c3a6d03b6e56dc17c35b1ac0
Luan Tedesco <luanfelipe2040@gmail.com>
2023-10-20 09:48:36 -0300

    reverted sessions

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,27 @@
-class SessionsController < Devise::SessionsController
-  before_action :check_status, only: [:create]
+# frozen_string_literal: true
 
-  private
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
 
-  def check_status
-    user = User.find_by(email: params[:user][:email])
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
 
-    return unless user && !user.status
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
 
-    flash[:alert] = 'Sua conta está desativada. Entre em contato com o suporte.'
-    redirect_to new_user_session_path
-  end
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
